f613e4702979871a85d3620b845860e63b4ea090
Sarikhin <sarikhinlovestory@gmail.com>
2025-03-07 23:10:03 +0700

    [update] update splash screen

--- app/src/main/java/id/my/indoquran/MainActivity.kt
+++ app/src/main/java/id/my/indoquran/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import id.my.indoquran.ui.theme.IndoQuranTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +17,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             IndoQuranTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    CustomSplashScreen(modifier = Modifier.padding(innerPadding) )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IndoQuranTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/id/my/indoquran/SplashScreen.kt
+++ app/src/main/java/id/my/indoquran/SplashScreen.kt
@@ -1,20 +1,126 @@
 package id.my.indoquran
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.my.indoquran.ui.theme.IndoQuranTheme
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
 
 @Composable
 fun CustomSplashScreen(modifier: Modifier = Modifier) {
+    var targetSizeOuter by remember { mutableStateOf(180.dp) } // Ukuran awal kecil
+    var targetSizeMiddle by remember { mutableStateOf(160.dp) }
+
+    // Memulai animasi setelah Composable dirender
+    LaunchedEffect(Unit) {
+        targetSizeOuter = 210.dp
+        targetSizeMiddle = 190.dp
+    }
+
+    val animatedSizeOuter by animateDpAsState(
+        targetValue = targetSizeOuter, // Animasi ke ukuran akhir
+        animationSpec = tween(durationMillis = 1000, easing = FastOutSlowInEasing),
+        label = "Outer Box Animation"
+    )
+
+    val animatedSizeMiddle by animateDpAsState(
+        targetValue = targetSizeMiddle, // Ukuran akhir
+        animationSpec = tween(durationMillis = 1000, delayMillis = 200, easing = FastOutSlowInEasing), // Delay 200ms
+        label = "Middle Box Animation"
+    )
+
     Box(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.primary),
         contentAlignment = Alignment.Center,
     ) {
-        Text("Splash Screen")
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+// animasi size box ini
+                modifier = Modifier
+                    .size(animatedSizeOuter)
+                    .clip(CircleShape)
+                    .background(Color.White.copy(alpha = 0.5f))
+                    .padding(20.dp)
+            ) {
+                Box(
+// animasi size box ini
+                    modifier = Modifier
+                        .size(animatedSizeMiddle)
+                        .clip(CircleShape)
+                        .background(Color.White.copy(alpha = 0.5f))
+                        .padding(20.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(100.dp)
+                            .clip(CircleShape)
+                            .background(Color.White.copy(alpha = 0.5f))
+                            .padding(20.dp)
+                            .align(Alignment.Center)
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.logo_indoquran),
+                            contentDescription = "Logo",
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                }
+            }
+        }
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                "IndoQur`an",
+                style = MaterialTheme.typography.titleLarge.copy(
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(
+                "Qur`an dan Terjemah",
+                style = MaterialTheme.typography.titleSmall.copy(
+                    fontWeight = FontWeight.Normal,
+                    color = Color.White
+                )
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+        }
     }
 }
 
@@ -22,5 +128,8 @@ fun CustomSplashScreen(modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun CustomSplashScreenPreview() {
-    CustomSplashScreen()
+    IndoQuranTheme {
+
+        CustomSplashScreen()
+    }
 }
