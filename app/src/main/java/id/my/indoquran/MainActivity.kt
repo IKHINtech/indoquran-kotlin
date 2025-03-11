382f332a3546a01ef26350ff2040f80a9f620475
Sarikhin <sarikhinlovestory@gmail.com>
2025-03-11 14:25:03 +0700

    [update] change icons

--- app/src/main/java/id/my/indoquran/MainActivity.kt
+++ app/src/main/java/id/my/indoquran/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            IndoQuranTheme {
+            IndoQuranTheme(darkTheme = false) {
                 MainScreen()
             }
         }
--- app/src/main/java/id/my/indoquran/MainScreen.kt
+++ app/src/main/java/id/my/indoquran/MainScreen.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -19,11 +19,12 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,13 +42,13 @@ import id.my.indoquran.ui.SurahScreen
 import id.my.indoquran.ui.theme.IndoQuranTheme
 
 
-enum class IndoQuranScreen() {
-    Home,
-    Quran,
-    Hadist,
-    Doa,
-    JadwalSholat,
-    SplashScreen
+enum class IndoQuranScreen(@DrawableRes title: Int) {
+    Home(title = R.string.home_screen),
+    Quran(title = R.string.quran_screen),
+    Hadist(title = R.string.hadits_screen),
+    Doa(title = R.string.doa_screen),
+    JadwalSholat(R.string.jadwal_sholat_screen),
+    SplashScreen(R.string.app_name)
 }
 
 data class BottomNavigationItem(
@@ -60,50 +61,45 @@ data class BottomNavigationItem(
 
 @ExperimentalMaterial3Api
 @Composable
-fun IndoQuranAppBar(
+fun IndoQuranMainAppBar(
     modifier: Modifier = Modifier,
-    currentScreen: IndoQuranScreen,
     navigateUp: () -> Unit = {},
     canNavigateBack: Boolean,
 ) {
-    TopAppBar(
-        title = {
-            Text(
-                "IndoQur`an",
-                style = MaterialTheme.typography.titleLarge.copy(
-                    color = MaterialTheme.colorScheme.primary,
-                    fontWeight = FontWeight.Bold,
-                )
+    TopAppBar(title = {
+        Text(
+            stringResource(R.string.app_name), style = MaterialTheme.typography.titleLarge.copy(
+                color = MaterialTheme.colorScheme.primary,
+                fontWeight = FontWeight.Bold,
             )
-        },
-        navigationIcon = {
-            if (canNavigateBack) {
-                IconButton(onClick = navigateUp) {
-                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back Button")
-                }
-            } else
-                Image(
-                    painter = painterResource(R.drawable.logo_indoquran),
-                    contentDescription = "Logo IndoQur`an",
-                    modifier = Modifier
-                        .size(
-                            60.dp
-                        )
-                        .padding(8.dp)
-                )
-        },
-        actions = {
-            IconButton(onClick = {}) {
+        )
+    }, navigationIcon = {
+        if (canNavigateBack) {
+            IconButton(onClick = navigateUp) {
                 Icon(
-                    imageVector = Icons.Filled.Settings, contentDescription = "Setting Icon",
-                    tint = MaterialTheme.colorScheme.primary,
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Back Button"
                 )
             }
-        },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.background
-        ),
-        modifier = modifier
+        } else Image(
+            painter = painterResource(R.drawable.logo_indoquran),
+            contentDescription = "Logo IndoQur`an",
+            modifier = Modifier
+                .size(
+                    60.dp
+                )
+                .padding(8.dp)
+        )
+    }, actions = {
+        IconButton(onClick = {}) {
+            Icon(
+                imageVector = Icons.Filled.Settings, contentDescription = "Setting Icon",
+                tint = MaterialTheme.colorScheme.primary,
+            )
+        }
+    }, colors = TopAppBarDefaults.topAppBarColors(
+        containerColor = MaterialTheme.colorScheme.background
+    ), modifier = modifier
     )
 }
 
@@ -115,10 +111,8 @@ fun IndoQuranAppBar(
 @Composable
 fun IndoQuranAppBarPreview() {
     IndoQuranTheme {
-        IndoQuranAppBar(
-            modifier = Modifier,
-            currentScreen = IndoQuranScreen.Quran,
-            canNavigateBack = false
+        IndoQuranMainAppBar(
+            modifier = Modifier, canNavigateBack = false
         )
     }
 }
@@ -128,10 +122,8 @@ fun IndoQuranAppBarPreview() {
 @Composable
 fun IndoQuranAppBarPreviewDark() {
     IndoQuranTheme(darkTheme = true) {
-        IndoQuranAppBar(
-            modifier = Modifier,
-            currentScreen = IndoQuranScreen.Quran,
-            canNavigateBack = false
+        IndoQuranMainAppBar(
+            modifier = Modifier, canNavigateBack = false
         )
     }
 }
@@ -139,62 +131,50 @@ fun IndoQuranAppBarPreviewDark() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(
-    modifier: Modifier = Modifier,
-    navController: NavHostController = rememberNavController()
+    modifier: Modifier = Modifier, navController: NavHostController = rememberNavController()
 ) {
-    var selectedItemIndex by rememberSaveable { mutableStateOf(0) }
+    var selectedItemIndex by rememberSaveable { mutableIntStateOf(0) }
     val items = listOf(
-        BottomNavigationItem(
-            title = "Home",
+        BottomNavigationItem(title = stringResource(R.string.home_screen),
             selectedIcon = R.drawable.home,
-            unSelectedIcon = R.drawable.home,
-            onClick = {navController.navigate(IndoQuranScreen.Home.name){
-                popUpTo(navController.graph.startDestinationRoute!!){inclusive = true}
-            } }
-        ),
-        BottomNavigationItem(
-            title = "Surah",
-            selectedIcon = R.drawable.quran,
-            unSelectedIcon = R.drawable.quran,
+            unSelectedIcon = R.drawable.home_outline,
+            onClick = {
+                navController.navigate(IndoQuranScreen.Home.name) {
+                    popUpTo(navController.graph.startDestinationRoute!!) { inclusive = true }
+                }
+            }),
+        BottomNavigationItem(title = stringResource(R.string.quran_screen),
+            selectedIcon = R.drawable.all_quran_fullfiled,
+            unSelectedIcon = R.drawable.al_quran_outline,
             onClick = {
                 navController.navigate(IndoQuranScreen.Quran.name) {
                     popUpTo(navController.graph.startDestinationRoute!!) { inclusive = true }
                 }
-            }
-        ),
-
-        BottomNavigationItem(
-            title = "Hadits",
-            selectedIcon = R.drawable.hadits,
-            unSelectedIcon = R.drawable.hadits,
+            }),
+        BottomNavigationItem(title = stringResource(R.string.hadits_screen),
+            selectedIcon = R.drawable.book_filled,
+            unSelectedIcon = R.drawable.book_outline,
             onClick = {
                 navController.navigate(IndoQuranScreen.Hadist.name) {
                     popUpTo(navController.graph.startDestinationRoute!!) { inclusive = true }
                 }
-            }
-        ),
-        BottomNavigationItem(
-            title = "Doa",
-            selectedIcon = R.drawable.doa,
-            unSelectedIcon = R.drawable.doa,
-
+            }),
+        BottomNavigationItem(title = stringResource(R.string.doa_screen),
+            selectedIcon = R.drawable.pray,
+            unSelectedIcon = R.drawable.pray_outline,
             onClick = {
                 navController.navigate(IndoQuranScreen.Doa.name) {
                     popUpTo(navController.graph.startDestinationRoute!!) { inclusive = true }
                 }
-            }
-        ),
-        BottomNavigationItem(
-            title = "Sholat",
+            }),
+        BottomNavigationItem(title = stringResource(R.string.jadwal_sholat_screen),
             selectedIcon = R.drawable.sholat,
-            unSelectedIcon = R.drawable.sholat,
-
+            unSelectedIcon = R.drawable.sholat_outline,
             onClick = {
                 navController.navigate(IndoQuranScreen.JadwalSholat.name) {
                     popUpTo(navController.graph.startDestinationRoute!!) { inclusive = true }
                 }
-            }
-        ),
+            }),
     )
 
     val backStackEntry by navController.currentBackStackEntryAsState()
@@ -202,47 +182,39 @@ fun MainScreen(
         backStackEntry?.destination?.route ?: IndoQuranScreen.SplashScreen.name
     )
 
-    Scaffold(
-        topBar = {
-            if (currentScreen != IndoQuranScreen.SplashScreen) {
-                IndoQuranAppBar(
-                    modifier = modifier,
-                    currentScreen = currentScreen,
-                    canNavigateBack = navController.previousBackStackEntry != null,
-                    navigateUp = { navController.navigateUp() })
-            }
-        },
-        bottomBar = {
-            if (currentScreen != IndoQuranScreen.SplashScreen) {
-                NavigationBar {
-                    items.forEachIndexed { index, item ->
-                        NavigationBarItem(
-                            selected = selectedItemIndex == index,
-                            onClick = {
-                                selectedItemIndex = index
-                            },
-                            alwaysShowLabel = false,
-                            label = { Text(text = item.title) },
-                            icon = {
-                                Icon(
-                                    modifier = Modifier.size(28.dp),
-                                    painter = if (selectedItemIndex == index) {
-                                        painterResource(item.selectedIcon)
-                                    } else painterResource(item.unSelectedIcon),
-                                    contentDescription = item.title,
-                                    tint = if (selectedItemIndex == index)
-                                        MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurface.copy(
-                                        alpha = 0.8f
-                                    ),
+    Scaffold(topBar = {
+        if (currentScreen != IndoQuranScreen.SplashScreen) {
+            IndoQuranMainAppBar(modifier = modifier,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() })
+        }
+    }, bottomBar = {
+        if (currentScreen != IndoQuranScreen.SplashScreen) {
+            NavigationBar {
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(selected = selectedItemIndex == index,
+                        onClick = {
+                            selectedItemIndex = index
+                        },
+                        alwaysShowLabel = false,
+                        label = { Text(text = item.title) },
+                        icon = {
+                            Icon(
+                                modifier = Modifier.size(28.dp),
+                                painter = if (selectedItemIndex == index) {
+                                    painterResource(item.selectedIcon)
+                                } else painterResource(item.unSelectedIcon),
+                                contentDescription = item.title,
+                                tint = if (selectedItemIndex == index) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurface.copy(
+                                    alpha = 0.8f
+                                ),
 
-                                    )
-                            }
-                        )
-                    }
+                                )
+                        })
                 }
             }
         }
-    ) { interPadding ->
+    }) { interPadding ->
         NavHost(
             navController = navController,
             startDestination = IndoQuranScreen.SplashScreen.name,
